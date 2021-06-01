4487ce7136dc571b7e331380959aa6af1a758ed3
Joshua Wray <joshua@getopenwater.com>
2021-06-01 11:29:54 -0500

    no message

---
+++ js/tableCal.js
@@ -0,0 +1,25 @@
+// to use: give table a class of sum-table and in the Field Template wrap the item to count in a span with sum-item as a class
+$.fn.extend({
+    createObserver: function(callback, config) {
+        config = config || {
+            childList: true
+        };
+        if (!!!window.systemObservers || !Array.isArray(window.systemObservers)) window.systemObservers = [];
+        var newObserver = new MutationObserver(callback);
+        newObserver.observe(this[0], config);
+        window.systemObservers.push(newObserver);
+        return this;
+    }
+});
+
+let $sumTable = $(".sum-table tbody");
+let $sumTotal = $(".sum-total input[type=text]");
+const calculateSumTotal = function() {
+    let total = 0;
+    $(".sum-item").each(function() {
+        total += Number(this.innerText);
+    });
+    $sumTotal.val(total).change();
+};
+
+$sumTable.createObserver(calculateSumTotal);
