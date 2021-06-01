c19a9090ce50d361fcfc38c10d6dc15ee4c636fe
Joshua Wray <joshua@getopenwater.com>
2021-06-01 11:34:20 -0500

    no message

--- js/tableCal.js
+++ js/tableCal.js
@@ -1,26 +1,29 @@
 // to use: give table a class of sum-table and in the Field Template wrap the item to count in a span with sum-item as a class. 
 // support for other operators coming soon
-$.fn.extend({
-    createObserver: function(callback, config) {
-        config = config || {
-            childList: true
-        };
-        if (!!!window.systemObservers || !Array.isArray(window.systemObservers)) window.systemObservers = [];
-        var newObserver = new MutationObserver(callback);
-        newObserver.observe(this[0], config);
-        window.systemObservers.push(newObserver);
-        return this;
-    }
-});
 
-let $sumTable = $(".sum-table tbody");
-let $sumTotal = $(".sum-total input[type=text]");
-const calculateSumTotal = function() {
-    let total = 0;
-    $(".sum-item").each(function() {
-        total += Number(this.innerText);
+FrontendApiManager.onReady("submissionForm", function(api) {
+    $.fn.extend({
+        createObserver: function(callback, config) {
+            config = config || {
+                childList: true
+            };
+            if (!!!window.systemObservers || !Array.isArray(window.systemObservers)) window.systemObservers = [];
+            var newObserver = new MutationObserver(callback);
+            newObserver.observe(this[0], config);
+            window.systemObservers.push(newObserver);
+            return this;
+        }
     });
-    $sumTotal.val(total).change();
-};
 
-$sumTable.createObserver(calculateSumTotal);
+    let $sumTable = $(".sum-table tbody");
+    let $sumTotal = $(".sum-total input[type=text]");
+    const calculateSumTotal = function() {
+        let total = 0;
+        $(".sum-item").each(function() {
+            total += Number(this.innerText);
+        });
+        $sumTotal.val(total).change();
+    };
+
+    $sumTable.createObserver(calculateSumTotal);
+});
