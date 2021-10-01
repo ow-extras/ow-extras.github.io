8782797c54e5649f64edf510639c6a29650a7672
Joshua Wray <joshua@getopenwater.com>
2021-09-30 22:14:08 -0500

    no message

--- setc/secondarySchoolTheatreFestivalTableCounters.js
+++ setc/secondarySchoolTheatreFestivalTableCounters.js
@@ -1,35 +1,35 @@
 FrontendApiManager.onReady("submissionForm", function(api) {
     function updateTotals() {
-        var numTeacher = api.getField("totalTeacherParticipants");
-        var numStu = api.getField("totalStudentParticipants");
-        var numCha = api.getField("totalChaperones");
-        var adultCount = (teacherCount = studentCount = 0);
+        var numTeacher = api.getField("totalTeacherParticipants")
+        var numStu = api.getField("totalStudentParticipants")
+        var numCha = api.getField("totalChaperones")
+        var adultCount = (teacherCount = studentCount = 0)
         if ($('.teacher').length == 0) {
-            numTeacher.setValue(0);
+            numTeacher.setValue(0)
         } else {
             $(".teacher").each(function() {
-                teacherCount++;
-                numTeacher.setValue(teacherCount);
-            });
+                teacherCount++
+                numTeacher.setValue(teacherCount)
+            })
         }
         if ($('.chaperone').length == 0) {
-            numCha.setValue(0);
+            numCha.setValue(0)
         } else {
             $(".chaperone").each(function() {
-                adultCount++;
-                numCha.setValue(adultCount);
-            });
+                adultCount++
+                numCha.setValue(adultCount)
+            })
         }
         if ($('.student').length == 0) {
-            numStu.setValue(0);
+            numStu.setValue(0)
         } else {
             $(".student").each(function() {
-                studentCount++;
-                numStu.setValue(studentCount);
-            });
+                studentCount++
+                numStu.setValue(studentCount)
+            })
         }
     }
     window.setInterval(function() {
-        updateTotals();
-    }, 1000);
-});
+        updateTotals()
+    }, 1000)
+})
