7ee583476e7beac2b6ce232f8333a21ceb8070c1
Joshua Wray <55522487+owjosh@users.noreply.github.com>
2022-04-08 11:05:20 -0500

    Update alt-autoCal.js

--- js/alt-autoCal.js
+++ js/alt-autoCal.js
@@ -7,18 +7,18 @@ FrontendApiManager.onReady("submissionForm", function(api) {
     let $multiplyFields = $('.alt-multiply-field');
     let $divideFields = $('.alt-divide-field');
     $.fn.extend({
-        sum: function() {
+        altsum: function() {
             return this.map((i, v) => Number(v.value)).toArray().reduce((t, i) => t + i);
         },
-        subtract: function() {
+        altsubtract: function() {
             const numberArray = this.map((i, v) => Number(v.value)).toArray();
             const firstValue = numberArray.shift();
             return numberArray.reduce((t, i) => t - i, firstValue);
         },
-        multiply: function() {
+        altmultiply: function() {
             return this.map((i, v) => Number(v.value)).toArray().reduce((t, i) => t * i);
         },
-        divide: function() {
+        altdivide: function() {
             return this.map((i, v) => Number(v.value)).toArray().reduce((t, i) => t / i);
         },
     });
@@ -26,25 +26,25 @@ FrontendApiManager.onReady("submissionForm", function(api) {
         let set = event.currentTarget.dataset.set;
         let $sumFields = $(`.alt-set-${set} input[type='text']`);
         let $sumDisplay = $(`.alt-set-${set}-total input[type='text']`);
-        $sumDisplay.val($sumFields.sum()).keyup();
+        $sumDisplay.val($sumFields.altsum()).keyup();
     };
     const keyUpSubtract = (event) => {
         let set = event.currentTarget.dataset.set;
         let $subtrahends = $(`.alt-set-${set}`).find('input[type=text]');
         let $sumDisplay = $(`.alt-set-${set}-total input[type='text']`);
-        $sumDisplay.val($subtrahends.subtract()).keyup();
+        $sumDisplay.val($subtrahends.altsubtract()).keyup();
     };
     const keyUpMultiply = (event) => {
         let set = event.currentTarget.dataset.set;
         let $factors = $(`.alt-set-${set} input[type='text']`);
         let $productDisplay = $(`.alt-set-${set}-total input[type='text']`);
-        $productDisplay.val($factors.multiply()).keyup();
+        $productDisplay.val($factors.altmultiply()).keyup();
     };
     const keyUpDivide = (event) => {
         let set = event.currentTarget.dataset.set;
         let $factors = $(`.alt-set-${set} input[type='text']`);
         let $productDisplay = $(`.alt-set-${set}-total input[type='text']`);
-        $productDisplay.val($factors.divide()).keyup();
+        $productDisplay.val($factors.altdivide()).keyup();
     };
     for (let i = 1; i <= MAX_SETS; i++) {
         fieldsObject[i] = $sumFields.filter('.alt-set-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpSum);
