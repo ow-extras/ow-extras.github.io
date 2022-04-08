b27b13a03d7957cdbcffd5181e4f9c67c570a5c2
Joshua Wray <55522487+owjosh@users.noreply.github.com>
2022-04-08 12:16:35 -0500

    Update alt-autoCal.js

--- js/alt-autoCal.js
+++ js/alt-autoCal.js
@@ -2,10 +2,10 @@ FrontendApiManager.onReady("submissionForm", function(api) {
     'use strict';
     let fieldsObject = {};
     const MAX_SETS = $(":input").length;
-    let $sumFields = $('.sum-alt-field');
-    let $subtractFields = $('.subtract-alt-field');
-    let $multiplyFields = $('.multiply-alt-field');
-    let $divideFields = $('.divide-alt-field');
+    let $sumField = $('.sumfield');
+    let $subtractField = $('.subtractfield');
+    let $multiplyField = $('.multiplyfield');
+    let $divideField = $('.dividefield');
     $.fn.extend({
         sum: function() {
             return this.map((i, v) => Number(v.value)).toArray().reduce((t, i) => t + i);
@@ -23,33 +23,33 @@ FrontendApiManager.onReady("submissionForm", function(api) {
         },
     });
     const keyUpSum = (event) => {
-        let set = event.currentTarget.dataset.setalt;
-        let $sumFields = $(`.setalt-${set} input[type='text']`);
-        let $sumDisplay = $(`.setalt-${set}-total input[type='text']`);
+        let set = event.currentTarget.dataset.set;
+        let $sumFields = $(`.group-${set} input[type='text']`);
+        let $sumDisplay = $(`.group-${set}-total input[type='text']`);
         $sumDisplay.val($sumFields.sum()).keyup();
     };
     const keyUpSubtract = (event) => {
-        let set = event.currentTarget.dataset.setalt;
-        let $subtrahends = $(`.setalt-${set}`).find('input[type=text]');
-        let $sumDisplay = $(`.setalt-${set}-total input[type='text']`);
+        let set = event.currentTarget.dataset.set;
+        let $subtrahends = $(`.group-${set}`).find('input[type=text]');
+        let $sumDisplay = $(`.group-${set}-total input[type='text']`);
         $sumDisplay.val($subtrahends.subtract()).keyup();
     };
     const keyUpMultiply = (event) => {
-        let set = event.currentTarget.dataset.setalt;
-        let $factors = $(`.setalt-${set} input[type='text']`);
-        let $productDisplay = $(`.setalt-${set}-total input[type='text']`);
+        let set = event.currentTarget.dataset.set;
+        let $factors = $(`.group-${set} input[type='text']`);
+        let $productDisplay = $(`.group-${set}-total input[type='text']`);
         $productDisplay.val($factors.multiply()).keyup();
     };
     const keyUpDivide = (event) => {
-        let set = event.currentTarget.dataset.setalt;
-        let $factors = $(`.setaltalt-${set} input[type='text']`);
-        let $productDisplay = $(`.setalt-${set}-total input[type='text']`);
+        let set = event.currentTarget.dataset.set;
+        let $factors = $(`.group-${set} input[type='text']`);
+        let $productDisplay = $(`.group-${set}-total input[type='text']`);
         $productDisplay.val($factors.divide()).keyup();
     };
     for (let i = 1; i <= MAX_SETS; i++) {
-        fieldsObject[i] = $sumFields.filter('.setalt-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpSum);
-        fieldsObject[i] = $subtractFields.filter('.setalt-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpSubtract);
-        fieldsObject[i] = $multiplyFields.filter('.setalt-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpMultiply);
-        fieldsObject[i] = $divideFields.filter('.setalt-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpDivide);
+        fieldsObject[i] = $sumField.filter('.group-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpSum);
+        fieldsObject[i] = $subtractField.filter('.group-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpSubtract);
+        fieldsObject[i] = $multiplyField.filter('.group-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpMultiply);
+        fieldsObject[i] = $divideField.filter('.group-' + i).find('input[type=text]').attr('data-set', i).keyup(keyUpDivide);
     };
 });
